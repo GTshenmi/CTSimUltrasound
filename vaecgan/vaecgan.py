396278566cfcdbe51ccca9d60723b10195ea73ef
GTshenmi <x936305695@gmail.com>
2023-02-22 14:11:24 +0800

    20230222_1200_rf2us3 1.修改 lr

--- vaecgan/vaecgan.py
+++ vaecgan/vaecgan.py
@@ -293,14 +293,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
     parser.add_argument("--batch_size", type=int, default=2, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.0005, help="adam: learning rate")
+    parser.add_argument("--lr", type=float, default=0.0003, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=256, help="size of each image dimension")
     parser.add_argument("--channels", type=int, default=1, help="number of image channels")
     parser.add_argument("--sample_interval", type=int, default=80, help="interval between image sampling")
-    parser.add_argument("--dataset_name", type=str, default="rf2us2", help="name of the dataset")
+    parser.add_argument("--dataset_name", type=str, default="rf2us3", help="name of the dataset")
     parser.add_argument("--rfdata_len", type=int, default=1024, help="length of rf data")
     parser.add_argument("--split_test", type=bool, default=True, help="if split test")
     parser.add_argument("--network", type=str, default="aecgan", help="if split test")
@@ -318,4 +318,5 @@ if __name__ == '__main__':
 #multi-stage discriminator
 #Coarse2fine
 #pix2pixHD
+
 
